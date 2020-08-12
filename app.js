c893da73bc35cd6e9b4d0339a7a6fe93b9baf2bf
Poon Jey-ren <jeyren95@hotmail.com>
2020-08-13 01:00:55 +0800

    Use ejs instead

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+const members = [
+  {
+    name: "Alex Nabiswa",
+    role: "Founder & Executive Director",
+    image: "images/team-members/alex-img.jpg"
+  }
+]
+
+const projects = [
+  {
+    projectName: "COVID-19 Response",
+    articleLink: "",
+    imageLink: "images/projects/covid-project.jpg"
+  },
+  {
+    projectName: "Big Data In Education",
+    articleLink: "",
+    imageLink: "images/projects/education-project.jpg"
+  },
+  {
+    projectName: "Fighting Hunger",
+    articleLink: "",
+    imageLink: "images/projects/education-project.jpg"
+  }
+]
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    projects: projects,
+    members: members
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server successfully running on port 3000")
+});
